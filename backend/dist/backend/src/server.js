330dbe0372ff0ce6fe4c1709a9c877c0f24ad3f8
michaelkrinningersg-coder <michael.krinninger.sg@gmail.com>
2026-06-14 11:22:22 +0200

    feat: add server entry point and native module resolution for packaged environment

--- backend/dist/backend/src/server.js
+++ backend/dist/backend/src/server.js
@@ -3,6 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+require("./prelude");
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const node_fs_1 = __importDefault(require("node:fs"));
